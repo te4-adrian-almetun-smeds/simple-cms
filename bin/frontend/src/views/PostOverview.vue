7e810849560adbb8cfe46377064e8bcdb47d3acc
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-09 19:40:32 +0100

    Updated post overview content

--- bin/frontend/src/views/PostOverview.vue
+++ bin/frontend/src/views/PostOverview.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
     <h1 class="h1-responsive">Post Overview</h1>
-    <div class="container" v-if="post != null && post.header != null">
-      <h2 class="h2-responsive">{{post.header}}</h2>
-      <a href="/edit">
-        <button type="button" class="btn btn-primary">
-          <i class="fas fa-trash mr-1"></i>
-          Edit
-        </button>
-      </a>
-      <a href="/abc">
+    <div class="container" v-if="post != null">
+      <div class="container">
+        <TextEditor :header="post.header" :data="post.body" buttonText="Update"/>
+      </div>
+      <router-link  :to="`/posts/${post.name}/delete`">
         <button type="button" class="btn btn-danger">
           <i class="fas fa-trash mr-1"></i>
           Delete
         </button>
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
-import {  Post } from "../main";
+import { Options, Vue } from "vue-class-component";
+import { Post } from "../main";
+import TextEditor from "@/components/TextEditor.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    TextEditor
+  }
+})
 
 export default class PostsOverview extends Vue {
   post: Post | null = null;
 
   async mounted?() {
     console.log('Hello world')
-    this.post = await new Post() // Create a post based on the provided postName
+    this.post = await Post.get(this.$route.params.postName) // Create a post based on the provided postName
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
