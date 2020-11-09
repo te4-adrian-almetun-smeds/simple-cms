97962e2671e75465507e930140a2b813d6c4df30
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-09 21:37:44 +0100

    Created button component

---
+++ bin/frontend/src/components/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button :type="type" class="btn {{classes}}" @click="addArticle">
+    <i v-if="icon !== ''" :class="`fas ${icon} mr-1`"></i>
+    {{text}}
+  </button>
+</template>
+
+<script lang="ts">
+
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Save'
+      },
+    type: {
+      type: String,
+      default: 'button'
+    },
+    classes: {
+      type: String,
+      default: 'btn-primary'
+    },
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
