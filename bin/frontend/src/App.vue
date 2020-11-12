626ef54bb4821a5b4d67bee50626ed89d84025b5
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-12 17:44:44 +0100

    Added header component

--- bin/frontend/src/App.vue
+++ bin/frontend/src/App.vue
@@ -1,23 +1,29 @@
 <template>
+  <Header />
   <div class="row">
     <Nav class="col-4" />
-    <router-view class="col-8"> </router-view>
+    <router-view class="col-8 content"> </router-view>
   </div>
 </template>
 
 <script lang="ts">
 import Nav from "./components/nav.vue";
+import Header from "./components/Header.vue";
 export default {
-  components: { Nav }
+  components: { Nav, Header }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+}
+.content {
+  padding-top: 4px;
 }
 </style>
---
+++ bin/frontend/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="white sticky-top">
+    <h3 class="text-dark h3-responsive">Simple CMS</h3>
+
+    <a
+      class="btn text-dark dropdown-toggle mr-4"
+      type="button"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      {{ currentBlog }}
+    </a>
+
+    <div v-if="blogs">
+      <div class="dropdown-menu" v-for="blog in blogs" :key="blog">
+        <a class="dropdown-item" href="#">Blog {{ blog.id }}</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </div>
+    </div>
+    <div v-else>
+      <div class="dropdown-menu">
+        <a class="dropdown-item">Create a Blog to get started</a>
+      </div>
+    </div>
+    <!--Slack-->
+    <button type="button" class="btn btn-md xyz justify-content-center align-items-center">
+      <i class="fas fa-user"></i>
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    blogs: {},
+    currentBlog: {
+      default: "No blog selected",
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  text-align: left;
+  .xyz {
+    width: 40px;
+  }
+  header {
+    padding: 4px;
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    width: 100%;
+    top: 0;
+    z-index: 99;
+    border-bottom: black 1px solid;
+  }
+}
+</style>
