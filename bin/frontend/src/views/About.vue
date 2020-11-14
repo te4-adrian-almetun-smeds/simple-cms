cfee9cde5e7e53440f3ed25e23b9f8e00f19ebae
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-14 10:26:09 +0100

    styling

--- bin/frontend/src/views/About.vue
+++ bin/frontend/src/views/About.vue
@@ -2,8 +2,35 @@
   <div>
     <h1 class="h1-responsive">About</h1>
     <div class="container">
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis placeat blanditiis hic tempore excepturi maxime voluptates ad eius dolor! Inventore voluptatum cum reiciendis nulla quia voluptates vero in cupiditate corrupti neque, earum soluta labore quod nam eveniet maiores quisquam quibusdam aspernatur maxime voluptatem quam dicta iure non. Quis explicabo laudantium autem, omnis voluptatibus odio excepturi repudiandae dolorem iste. Alias at placeat veritatis iste cumque excepturi eum voluptatum a adipisci, ducimus dolorem voluptates rem sint fuga libero quae perferendis consequuntur? Quaerat cumque maiores suscipit quia quo magni ipsam voluptatem. Nobis id cumque doloribus minima dolore sapiente placeat eius quam itaque reprehenderit.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum similique dolorum culpa temporibus in ipsam deleniti beatae earum adipisci molestiae maiores expedita eius libero fugit sit nihil tempore, cupiditate non! Nobis quod excepturi laudantium quas eius ab fugit. Sit tenetur qui illum illo laudantium rerum ab asperiores eos est cupiditate libero, harum odio cumque, nostrum voluptatum neque velit nisi voluptatem itaque nulla, sint fugit provident doloremque consectetur. Temporibus et ullam consequuntur iusto labore, cum itaque cupiditate at, vero, nostrum esse inventore dolorum. Quod quia veniam obcaecati dolor aspernatur nihil placeat temporibus sit. Dolorum veritatis, dolorem a aut aspernatur sed natus quis minus adipisci numquam cumque quam, odio expedita neque. Optio totam natus commodi aliquam, dolore, veniam ex debitis accusantium culpa voluptas maiores aut fugit consectetur repudiandae architecto. Minima, dolore maiores?</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis placeat
+        blanditiis hic tempore excepturi maxime voluptates ad eius dolor!
+        Inventore voluptatum cum reiciendis nulla quia voluptates vero in
+        cupiditate corrupti neque, earum soluta labore quod nam eveniet maiores
+        quisquam quibusdam aspernatur maxime voluptatem quam dicta iure non.
+        Quis explicabo laudantium autem, omnis voluptatibus odio excepturi
+        repudiandae dolorem iste. Alias at placeat veritatis iste cumque
+        excepturi eum voluptatum a adipisci, ducimus dolorem voluptates rem sint
+        fuga libero quae perferendis consequuntur? Quaerat cumque maiores
+        suscipit quia quo magni ipsam voluptatem. Nobis id cumque doloribus
+        minima dolore sapiente placeat eius quam itaque reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        similique dolorum culpa temporibus in ipsam deleniti beatae earum
+        adipisci molestiae maiores expedita eius libero fugit sit nihil tempore,
+        cupiditate non! Nobis quod excepturi laudantium quas eius ab fugit. Sit
+        tenetur qui illum illo laudantium rerum ab asperiores eos est cupiditate
+        libero, harum odio cumque, nostrum voluptatum neque velit nisi
+        voluptatem itaque nulla, sint fugit provident doloremque consectetur.
+        Temporibus et ullam consequuntur iusto labore, cum itaque cupiditate at,
+        vero, nostrum esse inventore dolorum. Quod quia veniam obcaecati dolor
+        aspernatur nihil placeat temporibus sit. Dolorum veritatis, dolorem a
+        aut aspernatur sed natus quis minus adipisci numquam cumque quam, odio
+        expedita neque. Optio totam natus commodi aliquam, dolore, veniam ex
+        debitis accusantium culpa voluptas maiores aut fugit consectetur
+        repudiandae architecto. Minima, dolore maiores?
+      </p>
     </div>
   </div>
 </template>
@@ -11,5 +38,5 @@
 <script lang="ts">
 export default {
   name: "About"
-}
+};
 </script>
