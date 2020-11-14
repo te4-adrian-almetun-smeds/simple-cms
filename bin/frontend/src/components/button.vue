f347b8144a45f76ea734031db52aea12fb8595ba
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-14 22:22:56 +0100

    Removed unused prop

--- bin/frontend/src/components/button.vue
+++ bin/frontend/src/components/button.vue
@@ -1,12 +1,11 @@
 <template>
-  <button :type="type" class="btn {{classes}}" @click="$emit('clicked')">
+  <button :type="type" class="btn" @click="$emit('clicked')">
     <i v-if="icon !== ''" :class="`fas ${icon} mr-1`"></i>
-    {{text}}
+    {{ text }}
   </button>
 </template>
 
 <script lang="ts">
-
 export default {
   name: "Button",
   props: {
@@ -17,10 +16,6 @@ export default {
     type: {
       type: String,
       default: "button"
-    },
-    classes: {
-      type: String,
-      default: "btn-primary"
     },
     icon: {
       type: String,
